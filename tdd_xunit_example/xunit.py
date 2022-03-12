3ecad794a4d4793b59428fc0b01eb41a52bf1118
norahyk <norahyk@gmail.com>
2022-03-12 18:09:16 +0900

    テストメソッドの呼び出しを親クラスに

--- tdd_xunit_example/xunit.py
+++ tdd_xunit_example/xunit.py
@@ -1,6 +1,29 @@
-from wasrun import WasRun
+from wasrun import TestCase, WasRun
 
-test = WasRun('testMethod')
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
+
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun('testMethod')
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+
+TestCaseTest('testRunning').run()
