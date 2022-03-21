87eee6088bbe14faeff7f11ff162daee2ac15f5d
norahyk <norahyk@gmail.com>
2022-03-21 16:56:04 +0900

    テストスイート追加

--- tdd_xunit_example/xunit.py
+++ tdd_xunit_example/xunit.py
@@ -1,29 +1,102 @@
-from wasrun import TestCase, WasRun
-
-
 class TestCase:
     def __init__(self, name):
         self.name = name
     
-    def run(self):
-        method = getattr(self, self.name)
-        method()
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    def run(self, result):
+        result.testStarted()
+        self.setUp()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except Exception as e:
+            print(e)
+            result.testFailed()
+        self.tearDown()
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+    
+    def testStarted(self):
+        self.runCount += 1
+    
+    def testFailed(self):
+        self.errorCount += 1
+    
+    def summary(self):
+        return f'{self.runCount} run, {self.errorCount} failed'
 
 class WasRun(TestCase):
-
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
+    
+    def setUp(self):
+        self.log = 'setUp '
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log += 'testMethod '
+    
+    def testBrokenMethod(self):
+        raise Exception
+    
+    def tearDown(self):
+        self.log += 'tearDown '
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+    
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
 
 class TestCaseTest(TestCase):
-    def testRunning(self):
+    def setUp(self):
+        self.result = TestResult()
+
+    def testTemplateMethod(self):
         test = WasRun('testMethod')
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        test.run(self.result)
+        assert('setUp testMethod tearDown ' == test.log)
+
+    def testResult(self):
+        test = WasRun('testMethod')
+        test.run(self.result)
+        assert('1 run, 0 failed' == self.result.summary())
+    
+    def testFailedResult(self):
+        test = WasRun('testBrokenMethod')
+        test.run(self.result)
+        assert('1 run, 1 failed' == self.result.summary())
+    
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert('1 run, 1 failed' == self.result.summary())
+    
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun('testMethod'))
+        suite.add(WasRun('testBrokenMethod'))
+        suite.run(self.result)
+        assert('2 run, 1 failed' == self.result.summary())
 
 
-TestCaseTest('testRunning').run()
+suite = TestSuite()
+suite.add(TestCaseTest('testTemplateMethod'))
+suite.add(TestCaseTest('testResult'))
+suite.add(TestCaseTest('testFailedResult'))
+suite.add(TestCaseTest('testFailedResultFormatting'))
+suite.add(TestCaseTest('testSuite'))
+result = TestResult()
+suite.run(result)
+print(result.summary())
